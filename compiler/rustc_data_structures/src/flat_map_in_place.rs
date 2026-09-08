5c862df9ea05467a43718331a19e8f1580b6be40
xmakro <xmakro@users.noreply.github.com>
2026-09-08 15:23:32 -0700

    Insert excess flat-map results in a single batch
    [skip ci]

--- compiler/rustc_data_structures/src/flat_map_in_place.rs
+++ compiler/rustc_data_structures/src/flat_map_in_place.rs
@@ -39,21 +39,23 @@ impl<V: FlatMapInPlaceVec> FlatMapInPlace<V::Elem> for V {
             while read_i < guard.0.len() {
                 // Move the read_i'th item out of the vector and map it to an iterator.
                 let e = ptr::read(guard.0.as_ptr().add(read_i));
-                let iter = f(e).into_iter();
+                let mut iter = f(e).into_iter();
                 read_i += 1;
 
-                for e in iter {
+                while let Some(e) = iter.next() {
                     if write_i < read_i {
                         ptr::write(guard.0.as_mut_ptr().add(write_i), e);
                         write_i += 1;
                     } else {
-                        // If this is reached we ran out of space in the middle of the vector.
-                        // However, the vector is in a valid state here, so we just do a somewhat
-                        // inefficient insert.
-                        guard.0.insert(write_i, e);
-
-                        read_i += 1;
-                        write_i += 1;
+                        // All holes are filled, so the vector is valid here. Insert the
+                        // remaining output together to avoid shifting the unvisited tail
+                        // separately for every element of a large expansion.
+                        let old_len = guard.0.len();
+                        guard.0.insert_many(write_i, std::iter::once(e).chain(iter));
+                        let inserted = guard.0.len() - old_len;
+                        read_i += inserted;
+                        write_i += inserted;
+                        break;
                     }
                 }
             }
@@ -80,7 +82,7 @@ pub unsafe trait FlatMapInPlaceVec {
     unsafe fn set_len(&mut self, len: usize);
     fn as_ptr(&self) -> *const Self::Elem;
     fn as_mut_ptr(&mut self) -> *mut Self::Elem;
-    fn insert(&mut self, idx: usize, elem: Self::Elem);
+    fn insert_many<I: Iterator<Item = Self::Elem>>(&mut self, idx: usize, elems: I);
 }
 
 unsafe impl<T> FlatMapInPlaceVec for Vec<T> {
@@ -104,8 +106,8 @@ unsafe impl<T> FlatMapInPlaceVec for Vec<T> {
         self.as_mut_ptr()
     }
 
-    fn insert(&mut self, idx: usize, elem: Self::Elem) {
-        self.insert(idx, elem);
+    fn insert_many<I: Iterator<Item = T>>(&mut self, idx: usize, elems: I) {
+        drop(self.splice(idx..idx, elems));
     }
 }
 
@@ -130,8 +132,8 @@ unsafe impl<T> FlatMapInPlaceVec for ThinVec<T> {
         self.as_mut_slice().as_mut_ptr()
     }
 
-    fn insert(&mut self, idx: usize, elem: Self::Elem) {
-        self.insert(idx, elem);
+    fn insert_many<I: Iterator<Item = T>>(&mut self, idx: usize, elems: I) {
+        drop(self.splice(idx..idx, elems));
     }
 }
 
@@ -156,7 +158,50 @@ unsafe impl<T, const N: usize> FlatMapInPlaceVec for SmallVec<[T; N]> {
         self.as_mut_ptr()
     }
 
-    fn insert(&mut self, idx: usize, elem: Self::Elem) {
-        self.insert(idx, elem);
+    fn insert_many<I: Iterator<Item = T>>(&mut self, idx: usize, elems: I) {
+        self.insert_many(idx, elems);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn compare_with_flat_map<V>()
+    where
+        V: FlatMapInPlace<u32> + FromIterator<u32> + AsRef<[u32]>,
+    {
+        for len in 0..20 {
+            for phase in 0..6 {
+                let expand = |x| (0..(x + phase) % 6).map(move |i| x * 100 + i);
+                let expected: Vec<_> = (0..len).flat_map(expand).collect();
+                let mut actual: V = (0..len).collect();
+                let mut visited = Vec::new();
+                actual.flat_map_in_place(|x| {
+                    visited.push(x);
+                    expand(x)
+                });
+                assert_eq!(visited, (0..len).collect::<Vec<_>>());
+                assert_eq!(actual.as_ref(), expected);
+
+                // A filter hides the exact length from bulk insertion.
+                let mut actual: V = (0..len).collect();
+                actual.flat_map_in_place(|x| expand(x).filter(|_| true));
+                assert_eq!(actual.as_ref(), expected);
+            }
+        }
+        // Include a large expansion before a long unvisited tail.
+        let mut actual: V = (0..100).collect();
+        actual.flat_map_in_place(|x| 0..if x == 0 { 1000 } else { 1 });
+        assert_eq!(actual.as_ref().len(), 1099);
+        assert_eq!(&actual.as_ref()[..1000], (0..1000).collect::<Vec<_>>());
+        assert!(actual.as_ref()[1000..].iter().all(|&x| x == 0));
+    }
+
+    #[test]
+    fn preserves_values_and_visit_order_for_all_containers() {
+        compare_with_flat_map::<Vec<u32>>();
+        compare_with_flat_map::<ThinVec<u32>>();
+        compare_with_flat_map::<SmallVec<[u32; 4]>>();
     }
 }
